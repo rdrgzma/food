8d92cc223d7964bebe3b7fa20287c55a97ab682c
Márcio Rodriguez de Rodrigues <rdrgzma@hotmail.com>
2021-04-14 23:25:08 -0300

    chances web route

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\PlanController;
 use App\Http\Controllers\Admin\AdminController;
 
-
-Route::get('admin', [AdminController::class, 'index'])->name('admin.home');
-Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
-Route::post('admin/plans', [PlanController::class, 'store'])->name('plans.store');
-Route::get('admin/plans/create', [PlanController::class, 'create'])->name('plans.create');
-Route::any('admin/plans/search', [PlanController::class, 'search'])->name('plans.search');
-Route::delete('admin/plans/{url}', [PlanController::class, 'destroy'])->name('plans.destroy');
-Route::get('admin/plans/{url}', [PlanController::class, 'show'])->name('plans.show');
-Route::put('admin/plans/{url}', [PlanController::class, 'update'])->name('plans.update');
-Route::get('admin/plans/{url}/edit', [PlanController::class, 'edit'])->name('plans.edit');
-
-
+Route::prefix('admin')
+                ->namespace('Admin')
+                ->group(function(){
+                    Route::get('/', [AdminController::class, 'index'])->name('admin.home');
+                    Route::get('plans', [PlanController::class, 'index'])->name('plans.index');
+                    Route::post('plans', [PlanController::class, 'store'])->name('plans.store');
+                    Route::get('plans/create', [PlanController::class, 'create'])->name('plans.create');
+                    Route::any('plans/search', [PlanController::class, 'search'])->name('plans.search');
+                    Route::delete('plans/{url}', [PlanController::class, 'destroy'])->name('plans.destroy');
+                    Route::get('plans/{url}', [PlanController::class, 'show'])->name('plans.show');
+                    Route::put('plans/{url}', [PlanController::class, 'update'])->name('plans.update');
+                    Route::get('plans/{url}/edit', [PlanController::class, 'edit'])->name('plans.edit');
+                });
 Route::get('/', function () {
     return view('welcome');
 });
